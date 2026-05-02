5869075468dbb2585eb5ea1a12f039a54abcd668
metrovian <paramedoct@gmail.com>
2026-05-03 00:11:28 +0900

    fix: normalize macos mac format

--- abstract/darwin/lookup.sh
+++ abstract/darwin/lookup.sh
@@ -1,5 +1,32 @@
 lookup_mac() {
-  arp -n "$1" | awk '/ at / {print $4; exit}'
+  local mac
+  local o1
+  local o2
+  local o3
+  local o4
+  local o5
+  local o6
+
+  mac="$(arp -n "$1" | awk '/ at / {print $4; exit}')"
+
+  if [[ -z "$mac" ]]; then
+    return
+  fi
+
+  IFS=: read -r o1 o2 o3 o4 o5 o6 <<<"$mac"
+
+  if [[ -z "$o1" || -z "$o2" || -z "$o3" || -z "$o4" || -z "$o5" || -z "$o6" ]]; then
+    echo "$mac"
+    return
+  fi
+
+  printf '%02x:%02x:%02x:%02x:%02x:%02x\n' \
+    "$((16#$o1))" \
+    "$((16#$o2))" \
+    "$((16#$o3))" \
+    "$((16#$o4))" \
+    "$((16#$o5))" \
+    "$((16#$o6))"
 }
 
 lookup_company() {
