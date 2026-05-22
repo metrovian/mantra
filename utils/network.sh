e6ac063f37a48648036e92325a71b6b2fb55a482
metrovian <paramedoct@gmail.com>
2026-05-22 20:57:58 +0900

    fix: trim network helpers

--- utils/network.sh
+++ utils/network.sh
@@ -1,32 +1,20 @@
-network_ip_to_int() {
+network_subnet_address() {
   local a
   local b
   local c
   local d
-  IFS=. read -r a b c d <<<"$1"
-  echo $((((a << 24) | (b << 16) | (c << 8) | d)))
-}
-
-network_int_to_ip() {
-  local value
-  value="$1"
-  echo "$(((value >> 24) & 255)).$(((value >> 16) & 255)).$(((value >> 8) & 255)).$((value & 255))"
-}
-
-network_prefix_mask() {
   local prefix
-  prefix="$1"
-  if ((prefix == 0)); then
-    echo 0
-    return
-  fi
-  echo $(((0xFFFFFFFF << (32 - prefix)) & 0xFFFFFFFF))
-}
-
-network_subnet_address() {
   local ip_int
   local mask_int
-  ip_int="$(network_ip_to_int "$1")"
-  mask_int="$(network_prefix_mask "$2")"
-  network_int_to_ip "$((ip_int & mask_int))"
+  local subnet_int
+  IFS=. read -r a b c d <<<"$1"
+  prefix="$2"
+  ip_int=$((((a << 24) | (b << 16) | (c << 8) | d)))
+  if ((prefix == 0)); then
+    mask_int=0
+  else
+    mask_int=$(((0xFFFFFFFF << (32 - prefix)) & 0xFFFFFFFF))
+  fi
+  subnet_int=$((ip_int & mask_int))
+  echo "$(((subnet_int >> 24) & 255)).$(((subnet_int >> 16) & 255)).$(((subnet_int >> 8) & 255)).$((subnet_int & 255))"
 }
