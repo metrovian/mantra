c851e77d5bb6efe3ff424272b2af11604f22223a
metrovian <paramedoct@gmail.com>
2026-06-06 00:40:33 +0900

    refactor: prefer stronger scanned host keys

--- utils/ssh.sh
+++ utils/ssh.sh
@@ -13,19 +13,16 @@ ssh_fingerprint_from_key() {
   printf '%s:%s\n' "$type" "$hash"
 }
 
-ssh_scan_key() {
-  local hostname
-  hostname=$1
-  shift
-  ssh-keyscan -T 2 "$@" "$hostname" 2>/dev/null \
-    | awk 'NF >= 3 && $1 !~ /^#/ && !found { print; found = 1 }
-      END { exit !found }'
-}
-
 ssh_capture_key() {
   local hostname
+  local output
   hostname=$1
-  ssh_scan_key "$hostname" -t ed25519 || ssh_scan_key "$hostname"
+  output="$(ssh-keyscan -T 2 "$hostname" 2>/dev/null)" || return 1
+  grep -m 1 -E '^[^#[:space:]]+[[:space:]]+ssh-ed25519[[:space:]]' <<<"$output" \
+    || grep -m 1 -E '^[^#[:space:]]+[[:space:]]+ecdsa-sha2-' <<<"$output" \
+    || grep -m 1 -E '^[^#[:space:]]+[[:space:]]+ssh-rsa[[:space:]]' <<<"$output" \
+    || awk 'NF >= 3 && $1 !~ /^#/ { print; found = 1; exit }
+      END { exit !found }' <<<"$output"
 }
 
 ssh_key_body() {
